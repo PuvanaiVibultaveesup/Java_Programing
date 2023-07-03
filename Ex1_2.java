ea16730c51490126791fa42c31a72d50dbdb6b2b
PuvanaiVibultaveesup <138434225+PuvanaiVibultaveesup@users.noreply.github.com>
2023-07-03 18:42:19 +0700

    Add files via upload

---
+++ Ex1_2.java
@@ -0,0 +1,5 @@
+public class Ex1_2 {
+    public static void main(String[] args) {
+        System.out.println("(9.5 x 4.5 - 2.5 x 3) / (45.5 - 3.5) = " +((9.5 * 4.5 - 2.5 * 3) / (45.5 -3.5)));
+    }
+}
