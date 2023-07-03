3b612a877fcc308eeb231d940ab4d2d69c5b7602
PuvanaiVibultaveesup <138434225+PuvanaiVibultaveesup@users.noreply.github.com>
2023-07-03 18:43:59 +0700

    Add files via upload

---
+++ Ex1_3.java
@@ -0,0 +1,8 @@
+public class Ex1_3 {
+    public static void main(String[] args) {
+        double width = 4.5;
+        double height = 7.9;
+        double area = width * height;
+        System.out.println("Area = " +width +" * " +height +" = " + area);
+    }
+}
